7382c4c16cec0acd5038c1753365b7b88c935b69
Harshil Ahir <harshilahir@outlook.com>
2025-09-08 18:59:38 -0400

    social media logo appearance with hyperlink

--- dark.js
+++ dark.js
@@ -40,3 +40,32 @@ darkModeToggle.addEventListener('click', () => {
     disableDarkMode();
   }
 })
+document.addEventListener("DOMContentLoaded", () => {
+  var body = document.querySelector("body");
+  const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+  let darkMode = localStorage.getItem("darkMode");
+
+  const enableDarkMode = () => {
+    body.classList.add("dark-mode");
+    localStorage.setItem("darkMode", "enabled");
+  };
+
+  const disableDarkMode = () => {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("darkMode", null);
+  };
+
+  if (darkMode === "enabled") {
+    enableDarkMode();
+  }
+
+  darkModeToggle.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode !== "enabled") {
+      enableDarkMode();
+    } else {
+      disableDarkMode();
+    }
+  });
+});
